9ca75da2792068b49356a4f34a7685c92ce8f009
Saollin <gregory.janosz@gmail.com>
2021-04-09 20:40:20 +0200

    Remove DbObject and related classes

--- spring-app/src/main/java/pl/edu/agh/springapp/DbInit.java
+++ spring-app/src/main/java/pl/edu/agh/springapp/DbInit.java
@@ -3,20 +3,11 @@ package pl.edu.agh.springapp;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-import pl.edu.agh.springapp.data.model.DbObject;
-import pl.edu.agh.springapp.repository.DbObjectRepository;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class DbInit implements CommandLineRunner {
-    private final DbObjectRepository repository;
-
     @Override
     public void run(String... args) throws Exception {
-        List<DbObject> dbObjects = Arrays.asList(new DbObject(0L, "a"), new DbObject(0L, "b"), new DbObject(0L, "c"));
-        repository.saveAll(dbObjects);
     }
 }
