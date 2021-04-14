acfbabf2459b6d7f1d1a9e6f27c5207593c3e054
Adrian <adrianwojciak@interia.pl>
2021-04-14 21:35:43 +0200

    api path as environment variable

--- react-app/src/api/requests.ts
+++ react-app/src/api/requests.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import { OneForOneOffer, Subject } from './models';
 
-export const getOneForOneOffers = () => axios.get<OneForOneOffer[]>('http://localhost:8080/api/one-to-one-offers');
+export const getOneForOneOffers = () => axios.get<OneForOneOffer[]>(process.env.REACT_APP_API_PATH + '/api/one-to-one-offers');
 
-export const deleteOneForOneOffer = (id: number) => axios.delete('http://localhost:8080/api/one-to-one-offers/' + id);
+export const deleteOneForOneOffer = (id: number) => axios.delete(process.env.REACT_APP_API_PATH + '/api/one-to-one-offers/' + id);
 
-export const getSubjects = () => axios.get<Subject[]>('http://localhost:8080/api/one-to-one-offers');
+export const getSubjects = () => axios.get<Subject[]>(process.env.REACT_APP_API_PATH + '/api/one-to-one-offers');
