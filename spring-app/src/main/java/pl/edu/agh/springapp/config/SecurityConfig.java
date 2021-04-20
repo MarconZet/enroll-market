40f9197a9f310ab9c7a14a44b3334fd9c73d91be
MarconZet <marcin.zlakowski@gmail.com>
2021-04-20 14:47:28 +0200

    Basic security

---
+++ spring-app/src/main/java/pl/edu/agh/springapp/config/SecurityConfig.java
@@ -0,0 +1,56 @@
+package pl.edu.agh.springapp.config;
+
+import lombok.RequiredArgsConstructor;
+import org.keycloak.adapters.KeycloakConfigResolver;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+import java.util.Arrays;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+    private final Environment environment;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) {
+        KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+        auth.authenticationProvider(keycloakAuthenticationProvider);
+    }
+
+    @Override
+    @Bean
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+
+    @Bean
+    public KeycloakConfigResolver keycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        boolean security = Arrays.asList(environment.getActiveProfiles()).contains("security");
+        if (security) {
+            super.configure(http);
+            http.authorizeRequests()
+                    .antMatchers("/api/**").hasRole("user")
+                    .anyRequest().permitAll();
+        } else {
+            http.authorizeRequests().anyRequest().permitAll();
+        }
+    }
+}
