de173458f9091e9b59de9239d998dcac22d1bc8d
Saollin <gregory.janosz@gmail.com>
2021-05-04 22:28:39 +0200

    Add paging to OneToOneOffers domain

--- spring-app/src/main/java/pl/edu/agh/springapp/domain/offer/OneToOneOfferController.java
+++ spring-app/src/main/java/pl/edu/agh/springapp/domain/offer/OneToOneOfferController.java
@@ -1,7 +1,12 @@
 package pl.edu.agh.springapp.domain.offer;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import pl.edu.agh.springapp.data.dto.offer.OfferDto;
 import pl.edu.agh.springapp.data.dto.offer.OneToOneOfferDto;
 import pl.edu.agh.springapp.data.dto.offer.OneToOneOfferPostDto;
 
@@ -18,8 +23,12 @@ public class OneToOneOfferController {
     }
 
     @GetMapping("/one-to-one-offers")
-    public List<OneToOneOfferDto> getAllOffers() {
-        return service.getAll();
+    public ResponseEntity<Page<OneToOneOfferDto>> getAllOffers(
+            @RequestParam(defaultValue = "0") Integer pageNo,
+            @RequestParam(defaultValue = "10") Integer pageSize
+    ) {
+        Page<OneToOneOfferDto> list = service.getAllOneToOneOffers(pageNo, pageSize);
+        return new ResponseEntity<>(list, new HttpHeaders(), HttpStatus.OK);
     }
 
     @DeleteMapping("/one-to-one-offers/{id}")
