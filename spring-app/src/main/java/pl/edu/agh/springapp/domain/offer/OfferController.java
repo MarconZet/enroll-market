78cbb1741f11e05dcdadf2b47e8b15b6d562a2cb
Saollin <gregory.janosz@gmail.com>
2021-05-09 12:50:36 +0200

    Add description of search criteria in swagger

--- spring-app/src/main/java/pl/edu/agh/springapp/domain/offer/OfferController.java
+++ spring-app/src/main/java/pl/edu/agh/springapp/domain/offer/OfferController.java
@@ -31,11 +31,28 @@ public class OfferController {
     }
 
     @GetMapping("/offers")
+    @ApiOperation(value = "Get list of offers - paging work, filtering (searching) - not")
     public ResponseEntity<Page<OfferDto>> getAllOffers(
             @RequestParam(defaultValue = "0") Integer pageNo,
-            @RequestParam(defaultValue = "10") Integer pageSize
+            @RequestParam(defaultValue = "10") Integer pageSize,
+            @ApiParam(value = "Used format: \n\n" +
+                    "`/?search=searchCriterion:value,searchCriterion2:value`\n\n" +
+                    "**Search criterion is one of:**\n" +
+                    "- subject (value is id of subject)\n" +
+                    "- courseType (value is one of enum: PROJECT, LABORATORY, LESSON)\n" +
+                    "- teacher (value is id of teacher)\n" +
+                    "- day (value is one of enum: MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY)\n\n" +
+                    "**Allowed operation:**\n" +
+                    "- : (Equality, =)\n" +
+                    "- ! (Negation, not equal)\n\n" +
+                    "**Example:**\n" +
+                    "`subject:1,courseType!PROJECT,teacher!2,day!MONDAY,day!WEDNESDAY`",
+                    example = "subject:1,courseType!PROJECT,teacher!2,day!MONDAY,day!WEDNESDAY"
+            )
+            @RequestParam(required = false) String search
     ) {
         Page<OfferDto> list = service.getAllOffers(pageNo, pageSize);
+        System.out.println(search);
         return new ResponseEntity<>(list, new HttpHeaders(), HttpStatus.OK);
     }
 
