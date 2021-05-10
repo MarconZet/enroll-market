8a5cb92432d58c81c5eb2d068a771d888f08e5e6
MarconZet <marcin.zlakowski@gmail.com>
2021-05-10 22:26:06 +0200

    Added option to CORS and new profile backend

--- spring-app/src/main/java/pl/edu/agh/springapp/security/user/DummyUser.java
+++ spring-app/src/main/java/pl/edu/agh/springapp/security/user/DummyUser.java
@@ -1,7 +1,9 @@
 package pl.edu.agh.springapp.security.user;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
 
+@SessionScope
 @Component
 public class DummyUser implements CurrentUser {
     @Override
