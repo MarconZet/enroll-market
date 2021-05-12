66a0bbda737db234b898ca02aac84b35130578a6
Adrian <adrianwojciak@interia.pl>
2021-05-12 23:01:14 +0200

    quick push

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -7,9 +7,10 @@ import AddOfferPage from './pages/AddOfferPage/AddOfferPage';
 import DataUploadAndDownloadPage from './pages/DataUploadAndDownloadPage/DataUploadAndDownloadPage';
 import OffersListingPage from './pages/OffersListingPage/OffersListingPage';
 import PrivateRoute from './components/PrivateRoute/PrivateRoute';
-// import { useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 // import { getUserDataRequest } from './store/userAuth/actions';
 import TimetablePage from "./pages/TimetablePage/TimetablePage";
+import { getGlobalDataRequest } from './store/globalData/actions';
 
 const IndexFiller: React.FC = () => (
 	<>
@@ -23,7 +24,7 @@ const NoFoundPageFiller: React.FC = () => (<h1>Tej strony jeszcze nie ma i możl
 const LoadingFiller: React.FC = () => (<h1>Poczekaj...</h1>);
 
 const App: React.FC = () => {
-	// const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
 	const initOptions = { pkceMethod: 'S256' };
 
@@ -31,6 +32,7 @@ const App: React.FC = () => {
 		if (event === 'onAuthSuccess') {
 			if (keycloak.authenticated && typeof keycloak?.token !== 'undefined') {
 				console.log('Auth!');
+				dispatch(getGlobalDataRequest());
 				// dispatch(getUserDataRequest(keycloak?.token));
 			}
 		}
